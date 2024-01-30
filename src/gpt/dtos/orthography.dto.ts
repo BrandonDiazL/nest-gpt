b4751d71417c9a93c5fba23caafc18c58ee20f65
Brandon Díaz <brandondiaz838@gmail.com>
2024-01-30 12:59:13 -0600

    Mi primer commit

---
+++ src/gpt/dtos/orthography.dto.ts
@@ -0,0 +1,11 @@
+import { IsOptional, IsString, IsInt } from "class-validator";
+
+export class OrthographyDto {
+
+    @IsString()
+    readonly prompt: string;
+
+    @IsInt()
+    @IsOptional()
+    readonly maxTokens?: number;
+}
